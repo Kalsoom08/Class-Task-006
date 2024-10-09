ac3cc07b2a2228ef4016be330e7b9c5e6da09bfe
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-10-09 16:46:05 +0500

    Fields are accessed In JS

---
+++ script.js
@@ -0,0 +1,4 @@
+var getWeight = document.getElementById("weight");
+var getHeight = document.getElementById("height");
+var butn = document.getElementById("btn");
+var outPut = document.getElementById("output");
