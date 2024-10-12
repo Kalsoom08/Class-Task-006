721a45e857eb2133b98c5299b8e4acbca4cdddde
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-10-12 11:35:27 +0500

    Animations are Added

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+//Getting Elements using DOM
 var butn = document.getElementById("btn");
 var outPut = document.getElementById("output");
 var form = document.getElementsByTagName('form')[0]; 
 var resetBtn = document.getElementById("resetBtn")
 
+//Add event Listener 
 
 butn.addEventListener('click', function(event){
 event.preventDefault();
 let getWeight = +document.getElementById("weight").value;
 let getHeight = +document.getElementById("height").value;
 
+//Adding Conditions to to prrevent Illegal Inputs
 if (isNaN(getWeight) || isNaN(getHeight) || getWeight <= 0 || getHeight <= 0) {
   outPut.innerHTML = "Please enter valid positive numbers for weight and height.";
   return;
 }
 
+//Calculating Result
 let result = getWeight/(getHeight * getHeight)
 
-
-
+//Cheking The BMI Conditions
 if (result < 18.5) {
   outPut.innerHTML = `Your BMI is: ${result.toFixed(2)}. You are Underweight.`;
 } else if (result >= 18.5 && result <= 24.9) {
@@ -28,6 +31,7 @@ if (result < 18.5) {
   outPut.innerHTML = `Your BMI is: ${result.toFixed(2)}. You are Obese.`;
 }} )
 
+//Adding Reset Buttn to Clear The Form
 resetBtn.addEventListener('click', function() {
   let getWeight = document.getElementById("weight").value;
   let getHeight = document.getElementById("height").value;
